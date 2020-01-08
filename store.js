c8a6154401f4e9389d6d03efeb2e4a84a2d00945
praketa <praketasxena@gmail.com>
2020-01-08 02:54:48 -0600

    Added required files

---
+++ store.js
@@ -0,0 +1,21 @@
+var data =[
+    {id:1,q:'Who was the only unanimously-elected president?',a:'George Washington',d:''},
+    {id:2,q:'Who was the country’s first-known speleologist (cave explorer)?',a:'Thomas Jefferson',d:''},
+    {id:3,q:'Who was the smallest of all the presidents?',a:'James Madison',d:' He was only 5′4″ tall and weighed less than 100 pounds.'},
+    {id:4,q:'Who was the first president to live in a "white", White House?',a:'James Monroe',d:''},
+    {id:5,q:'Who was the first president to be interviewed by a woman journalist, naked?',a:'John Quincy Adams',d:' He customarily took a nude early morning swim in the Potomac River. Anne Royall, the first U.S. professional journalist, knew of his 5:00 AM swims. After being refused interviews with the president time after time, she went to the river, gathered his clothes and sat on them until she had her interview. Before this, no female had interviewed a president (least of all naked!).'},
+    {id:6,q:'Which president played the violin, loved to dance, spoke softly, and had good manners?',a:'John Tyler',d:' He played the violin, loved to dance, spoke softly, and had good manners.'},
+    {id:7,q:'Which president played is regarded as the most succussful?',a:'James Polk',d:' He was the most successful president in American history. During the 1844 campaign, he made five promises: to acquire California from Mexico, to settle the Oregon dispute, to lower the tariff, to establish a sub-treasury, and to retire from the office after four years. When he left office, his campaign promises had all been fulfilled.'},
+    {id:8,q:'Who was the first president to have a Christmas tee in the White house?',a:'Franklin Pierce',d:''},
+    {id:9,q:'Who was the first president to have a patent in his name?',a:'Abraham Lincoln',d:' He patented his floating drydock on May 22, 1849, patent #6469. He was the first U.S. President to receive a patent.'},
+    {id:10,q:'Which president was buried and wrapped in a U.S. flag and with his well-worn copy of the Constitution under his head?',a:'Andrew Johnson',d:' He was buried wrapped in a U.S. flag and with his well-worn copy of the Constitution under his head.'},          
+]
+
+
+var falseAnswers=[{id:"gW"}, "lincoln","monroe","quincy"]
+
+
+
+
+
+console.log(data)
